d4ce52eae5089875e47a8d889f84b5bea0e93118
googlegis <googlegis@hotmail.com>
2014-10-13 23:55:41 +0800

    complete the app files

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import QuartzCore
+
 
 class ViewController: UIViewController {
 
@@ -121,6 +123,12 @@ class ViewController: UIViewController {
     @IBAction func startOver(){
         startNewGame()
         updateLabels()
+        
+        let transition = CATransition()
+        transition.type = kCATransitionFade
+        transition.duration = 1
+        transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        view.layer.addAnimation(transition, forKey: nil)
     }
 }
 
