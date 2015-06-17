1f9236b417cf106cd20be7ecaa9df0f6294cb3f7
googlegis <googlegis@hotmail.com>
2015-06-17 23:40:30 +0800

    new project

--- BullsEyeold/BullsEye/AboutViewController.swift
+++ BullsEyeold/BullsEye/AboutViewController.swift
@@ -18,7 +18,7 @@ class AboutViewController: UIViewController {
         if let htmlFile = NSBundle.mainBundle().pathForResource("BullsEye", ofType: "html") {
             let htmlData = NSData(contentsOfFile: htmlFile)
             let baseURL = NSURL.fileURLWithPath(NSBundle.mainBundle().bundlePath)
-            webView.loadData(htmlData, MIMEType: "text/html", textEncodingName: "UTF-8", baseURL: baseURL)
+            webView.loadData(htmlData!, MIMEType: "text/html", textEncodingName: "UTF-8", baseURL: baseURL)
             
         }
         
