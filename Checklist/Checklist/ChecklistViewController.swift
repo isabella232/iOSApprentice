0ae063c405df6b39402f827777d0186f9e993c53
googlegis <googlegis@hotmail.com>
2015-06-29 23:05:21 +0800

    add checklist

---
+++ Checklist/Checklist/ChecklistViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Checklist
+//
+//  Created by Jay on 15/6/29.
+//  Copyright © 2015年 look4us. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("ChecklistItem") as UITableViewCell!
+        
+        return cell
+    }
+
+
+}
+
