382ef719a0ccee8ef5468943f73328b69f6ed100
googlegis <googlegis@hotmail.com>
2015-12-01 21:44:02 +0800

    set search bar location

---
+++ StoreSearch/StoreSearch/SearchViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Jay on 15/11/30.
+//  Copyright © 2015年 look4us. All rights reserved.
+//
+
+import UIKit
+
+
+class SearchViewController: UIViewController {
+
+    @IBOutlet weak var searchBar:UISearchBar!
+    
+    @IBOutlet weak var tableView:UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
+    }
+
+    /**
+     内存提醒
+     */
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+
+extension SearchViewController:UISearchBarDelegate {
+
+    /**
+     查询栏输入事件
+     
+     - parameter searchBar: 查询栏
+     */
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        
+        print("The search text is:'\(searchBar.text!)'")
+    }
+    
+
+}
+
+extension SearchViewController:UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
+
+}
+
+extension SearchViewController:UITableViewDelegate {
+
+    
+
+}
+
