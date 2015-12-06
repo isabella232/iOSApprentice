4d8c1086afc2981071cbf01ae8960535895adcb7
googlegis <googlegis@hotmail.com>
2015-12-06 20:30:58 +0800

    他们是Gay---烈日灼心

--- StoreSearch/StoreSearch/SearchResult.swift
+++ StoreSearch/StoreSearch/SearchResult.swift
@@ -13,4 +13,19 @@ class SearchResult {
     var name = ""
     
     var artistName = ""
+    
+    var artworkURL60 = ""
+    
+    var artworkURL100 = ""
+    
+    var storeURL = ""
+    
+    var kind = ""
+    
+    var currency = ""
+    
+    var price = 0.0
+    
+    var genre = ""
+    
 }
--- StoreSearch/StoreSearch/SearchViewController.swift
+++ StoreSearch/StoreSearch/SearchViewController.swift
@@ -55,7 +55,133 @@ class SearchViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func urlWithSearchText(searchText:String)->NSURL {
+    
+        let escapedSearchText = searchText.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+        
+        let urlString = String(format: "https://itunes.apple.com/search?term=%@", escapedSearchText!)
+        
+        let url = NSURL(string: urlString)
+        
+        return url!
+        
+    }
+    
+    func performStoreRequestWithURL(url:NSURL) -> String?{
+    
+        do {
+            
+            return try String(contentsOfURL: url, encoding: NSUTF8StringEncoding)
+        } catch {
+        
+            print("Download Error:\(error)")
+            
+            return nil
+        
+        
+        }
+    
+    }
+    
+    func parseJson(jsonString: String) -> [String:AnyObject]? {
+    
+        
+        guard let data = jsonString.dataUsingEncoding(NSUTF8StringEncoding)
+        
+            else {return nil}
+        
+        do {
+        
+            return try NSJSONSerialization.JSONObjectWithData(data, options: []) as? [String:AnyObject]
+        
+        } catch{
+        
+            print("Json Error:\(error)")
+            
+            return nil
+        }
+    
+    }
+    
+    func showNetworkError(){
+    
+        let alert = UIAlertController(
+            title: "Whoops...", message: "There was an error reading from the iTunes Store.please try again .", preferredStyle: .Alert)
+        
+        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alert.addAction(action)
+        
+        presentViewController(alert, animated: true, completion: nil)
+    
+    }
+    
+    
+    func parseDictionary(dictionary:[String:AnyObject]) -> [SearchResult]{
+    
+        guard let array = dictionary["results"] as? [AnyObject] else {
+        
+            print("Excepted 'results' array")
+            
+            return []
+        }
+        
+        var searchResults = [SearchResult]()
+        
+        for resultDict in array {
+            
+            if let resultDict = resultDict as? [String:AnyObject]{
+            
+                var searchResult: SearchResult?
+                
+                if let wrapperType = resultDict["wrapperType"] as? String {
+                    
+                    
+                    switch wrapperType {
+                    
+                    case "track":
+                        searchResult = parseTrack(resultDict)
+                    default:
+                        break;
+                    }
+                }
+                
+                if let result = searchResult {
+                    searchResults.append(result)
+                }
+            }
+        }
+        
+        return searchResults
+        
+    }
+    
+    func parseTrack(dictionary:[String:AnyObject]) -> SearchResult {
+        
+        let searchResult = SearchResult()
+        
+        searchResult.name = dictionary["trackName"] as! String
+        searchResult.artistName = dictionary["artistName"] as! String
+        searchResult.artworkURL60 = dictionary["artworkUrl60"] as! String
+        searchResult.artworkURL100 = dictionary["artworkUrl100"] as! String
+        searchResult.storeURL = dictionary["trackViewUrl"] as! String
+        searchResult.kind = dictionary["kind"] as! String
+        searchResult.currency = dictionary["currency"] as! String
+        
+        if let price = dictionary["trackPrice"] as? Double {
+        
+            searchResult.price = price
+        
+        }
+        
+        if let genre = dictionary["primaryGenreName"] as? String {
+        
+            searchResult.genre = genre
+        
+        }
+        
+        return searchResult
+    
+    }
 }
 
 
@@ -68,28 +194,35 @@ extension SearchViewController:UISearchBarDelegate {
      */
     func searchBarSearchButtonClicked(searchBar: UISearchBar) {
         
-        searchBar.resignFirstResponder()
-        
-        hasSearched = true
-        
-        searchResults = [SearchResult]()
-
-        if searchBar.text! != "justin bieber"{
+        if !searchBar.text!.isEmpty {
             
-            for i in 0...2 {
-                
-                let searchResult = SearchResult()
-                
-                searchResult.name = String(format: "Fake Result %d for ",i)
-                
-                searchResult.artistName = searchBar.text!
-                
-                searchResults.append(searchResult)
-                
+            searchBar.resignFirstResponder()
+            
+            hasSearched = true
+            
+            searchResults = [SearchResult]()
+            
+            let url = urlWithSearchText(searchBar.text!)
+            
+            print("URL:\(url)")
+            
+            if let jsonString = performStoreRequestWithURL(url){
+            
+                if let dictionary = parseJson(jsonString) {
+                    
+                    print("Dictionary \(dictionary)")
+                    
+                    searchResults = parseDictionary(dictionary)
+                    
+                    tableView.reloadData()
+                    return
+                }
+            
             }
+            showNetworkError()
+            
         }
         
-        tableView.reloadData()
     }
 
 }
@@ -127,8 +260,14 @@ extension SearchViewController:UITableViewDataSource {
             
             cell.nameLabel.text = searchResult.name
             
-            cell.artistNameLabel.text = searchResult.artistName
             
+            if searchResult.artistName.isEmpty {
+            
+                cell.artistNameLabel.text = "unknown"
+            } else {
+            
+                cell.artistNameLabel.text = String(format: "%@ (%@)", searchResult.artistName, searchResult.kind)
+            }
              return cell
         
         }
